1fce4d9b7e12e5c597fc56dac96c66ccf267a1b5
kzt hrk <kzt.hiraoka@gmail.com>
2021-10-12 14:33:11 +0900

    pypi package

--- setup.py
+++ setup.py
@@ -6,13 +6,15 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="japan_address",
     version="0.7.0",
+    license='MIT',
     author="kzthrk",
     author_email="kzthrk@gmail.com",
-    description="Japan address Utilities",
+    description="Japan address utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kzthrk/japan_address",
     packages=setuptools.find_packages(),
+    keywords='japan, japanese, address',
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
